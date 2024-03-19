680be1eb824019b11bc0e2ec74ecf1aa57c22c7e
Фотима <157792929+osiyo0709@users.noreply.github.com>
2024-03-19 14:51:27 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+String.prototype.replaceAll = function(search, replacement){
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+function lotindan_kirilga(text){
+    var lotin = text
+    .replaceAll("A","A")
+    .replaceAll("B","Б")
+    .replaceAll("D","Д")
+    .replaceAll("E","Э")
+    .replaceAll("F","Ф")
+    .replaceAll("G","Г")
+    .replaceAll("H","Ҳ")
+    .replaceAll("I","И")
+    .replaceAll("J","Ж")
+    .replaceAll("K","К")
+    .replaceAll("L","Л")
+    .replaceAll("M","М")
+    .replaceAll("N","Н")
+    .replaceAll("O","O")
+    .replaceAll("P","П")
+    .replaceAll("Q","Қ")
+    .replaceAll("R","Р")      
+    .replaceAll("S","С")
+    .replaceAll("T","Т")
+    .replaceAll("U","У")
+    .replaceAll("V","В")
+    .replaceAll("X","X")
+    .replaceAll("Y","Й")
+    .replaceAll("Z","З")
+    .replaceAll("O'","Ў")
+    .replaceAll("G'","Ғ")
+    .replaceAll("SH","Ш")
+    .replaceAll("CH","Ч")
+    .replaceAll("NG","Ь")
+
+    .replaceAll("a","a")
+    .replaceAll("b","б")
+    .replaceAll("d","д")
+    .replaceAll("e","э")
+    .replaceAll("f","ф")
+    .replaceAll("g","г")
+    .replaceAll("h","ҳ")
+    .replaceAll("i","и")
+    .replaceAll("j","ж")
+    .replaceAll("k","к")
+    .replaceAll("l","л")
+    .replaceAll("m","м")
+    .replaceAll("n","н")
+    .replaceAll("o","o")
+    .replaceAll("p","п")
+    .replaceAll("q","қ")
+    .replaceAll("r","р")      
+    .replaceAll("s","с")
+    .replaceAll("t","т")
+    .replaceAll("u","у")
+    .replaceAll("v","в")
+    .replaceAll("x","Х")     
+    .replaceAll("y","й")
+    .replaceAll("z","з")
+    .replaceAll("o'","ў")
+    .replaceAll("g'","ғ")
+    .replaceAll("sh","ш")
+    .replaceAll("ch","ч")
+    .replaceAll("ng","ь")
+    return lotin;
+}
+function tarjima(){
+    var input = document.getElementById("lotinmatn").value;
+    var output = document.getElementById("kirilmatn").innerHTML = lotindan_kirilga(input);
+}
